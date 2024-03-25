28710377521f9e423f1a598aa617703ba2aab156
AramMelkonyan <162093036+AramMelkonyan@users.noreply.github.com>
2024-03-25 19:21:19 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+import argparse
+import sys
+
+def init():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--name", help = "test case path", required = True)
+    parser.add_argument("--input", help = "test inputs file path", required = True)
+    parser.add_argument("--output", help = "test outputs file path", required = True)
+    parser.add_argument("--golden", help = "golden file path for comparison", required = True)
+    args = parser.parse_args()
+    return args.name, args.input, args.output, args.golden
+
+
+def xor_files(result_path, golden_path):
+    res = "PASS"
+    try:
+        with open (result_path, 'r') as f1:
+            try:
+                with open (golden_path, 'r') as f2:
+                    result_arr = f1.read()
+                    golden_arr = f2.read()
+                    if len(result_arr) == len(golden_arr):
+                        for i in range(len(result_arr)):
+                            if result_arr[i] != golden_arr[i]:
+                                res = "FAIL"
+                                break
+                    else:
+                        res = "FAIL"
+                    print(res)
+            except IOError:
+                print(f"Cannot open result file: {golden_path}")
+    except IOError:
+        print(f"Cannot open golden file: {result_path}")
+
+
+def main():
+    test_path, input_path, output_path, golden_path = init()
+    sys.path.append(test_path)
+    from tester import Evaluator
+    try:
+        with open(input_path, 'r') as f_in:
+            try:
+                with open(output_path, 'w') as f_out:
+                    evaluator = Evaluator(f_out)
+                    # all arguments in one string
+                    for args_str in f_in.readlines():
+                       evaluator.evaluate(args_str)
+            except IOError:
+                print(f"Cannot open output file: {output_path}")
+    except IOError:
+        print(f"Cannot open input file: {input_path}")
+        exit(1)
+    xor_files(output_path, golden_path)
+
+
+if __name__ == "__main__":
+    main()
+    # input("please enter to exit")
